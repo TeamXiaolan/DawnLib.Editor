d970f36064b9c802f1aae07c6988313bc5537fd9
XuuXiao <ammaroaladel@Gmail.com>
2025-08-25 10:42:23 +0100

    im surprised this worked first try

---
+++ CodeRebirthLib.Editor/src/Extensions/SerializedPropertyExtensions.cs
@@ -0,0 +1,78 @@
+
+using System;
+using System.Collections;
+using System.Reflection;
+using UnityEditor;
+
+namespace CodeRebirthLib.Editor.Extensions;
+static class SerializedPropertyExtensions
+{
+    public static object? GetTargetObjectOfProperty(this SerializedProperty prop)
+    {
+        if (prop == null)
+        {
+            return null;
+        }
+
+        string path = prop.propertyPath.Replace(".Array.data[", "[");
+        object? obj = prop.serializedObject.targetObject;
+        string[] elements = path.Split('.');
+
+        foreach (var element in elements)
+        {
+            if (element.Contains("["))
+            {
+                string elementName = element[..element.IndexOf("[")];
+                int index = Convert.ToInt32(element[element.IndexOf("[")..].Trim('[', ']'));
+                obj = GetValue_Imp(obj!, elementName, index);
+            }
+            else
+            {
+                obj = GetValue_Imp(obj!, element);
+            }
+        }
+        return obj;
+    }
+
+    static object? GetValue_Imp(object source, string name)
+    {
+        if (source == null)
+        {
+            return null;
+        }
+
+        Type? type = source.GetType();
+        while (type != null)
+        {
+            FieldInfo? fieldInfo = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (fieldInfo != null)
+            {
+                return fieldInfo.GetValue(source);
+            }
+
+            PropertyInfo? propertyInfo = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (propertyInfo != null)
+            {
+                return propertyInfo.GetValue(source, null);
+            }
+            type = type.BaseType;
+        }
+        return null;
+    }
+
+    static object? GetValue_Imp(object source, string name, int index)
+    {
+        if (GetValue_Imp(source, name) is not IEnumerable enumerable)
+            return null;
+
+        IEnumerator enm = enumerable.GetEnumerator();
+        for (int i = 0; i <= index; i++)
+        {
+            if (!enm.MoveNext())
+            {
+                return null;
+            }
+        }
+        return enm.Current;
+    }
+}
---
+++ CodeRebirthLib.Editor/src/PropertyDrawers/EntityDataDrawer.cs
@@ -0,0 +1,28 @@
+using CodeRebirthLib.CRMod;
+using CodeRebirthLib.Editor.Extensions;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EntityData), true)]
+public class EntityDataDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        object? obj = property.GetTargetObjectOfProperty();
+        if (obj is EntityData entity)
+        {
+            string? keyLabel = entity.Key?.ToString();
+            if (!string.IsNullOrEmpty(keyLabel))
+            {
+                label = new GUIContent(keyLabel);
+            }
+        }
+
+        EditorGUI.PropertyField(position, property, label, true);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+}
